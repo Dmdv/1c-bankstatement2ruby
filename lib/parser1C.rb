5939d4fc95e186fb3e7b0bd61c4070b0a6bfe8bd
Юрий Изотов <rubyonrailsmaster@gmail.com>
2013-04-03 20:09:53 +0600

    Доработан парсинг шапки и рассчетных счетов

--- lib/parser1C.rb
+++ lib/parser1C.rb
@@ -4,11 +4,8 @@ require 'iconv'
 
 class Parser1C
 
-attr_reader :file_name, :file_path, :format_version
+attr_reader :file_name, :file_path, :parameters, :accounts
 
-
-@session_parameters = Array.new #общие параметры сессии, берутся из файла обмена
-@docs = Array.new        #массив готовых. Каждый из них - хеш.  
 @file_name = String.new  #имя входного файла
 @file_path = String.new  #полный путь к файлу
 
@@ -23,35 +20,73 @@ def initialize(filename)
 end
 
 def parse #здесь и происходит парсинг
-
+@accounts = Hash.new     #рассчетные счета
+@parameters = Hash.new   #Шапка с параметрами передачи и т.д.
 converter= Iconv.new("UTF-8","WINDOWS-1251")
+temporary_account = Hash.new #поглощает в себя секцию рассчетного счета
+    #флаги секций
+   in_acc_section = false #СекцияРасчСчет
+   do_not_parse_header = false #пересекли ли мы хедер уже, флаг не дает затереть хедер дальнейшими директивами у каких совпадают имена
+    #конец флагов секций   
 File.open(@file_path, "r") do |input|
     input.each {|current_string_source|
-    current_string = converter.iconv(current_string_source)
+    current_string = converter.iconv(current_string_source).strip
     
-    if current_string.include?('=')
+    if current_string.include?('=') #если происходит уравнение, значит мы внутри некоей секции или шапки, и получаем некие параметры 
+
       current_pair = current_string.split('=')
       req = current_pair.first.strip       
       req_value = current_pair.last.strip 
-#      puts "Параметр #{req} равен #{req_value}"     
-      case req
-      when "ВерсияФормата" then @format_version = req_value
-      when "Кодировка"     then @encoding = req_value
-      when "Получатель"    then @recipient = req_value  
-      when "Отправитель"   then @sender = req_value
-      when "ДатаСоздания"  then @create_date = req_value
-      when "ВремяСоздания" then @create_time = req_value
-      when "ДатаНачала"    then @date_begin  = req_value
-      when "ДатаКонца"     then @date_end = req_value        
-      end 
-    end 
-    }
+     
+ 
+      #если мы в секции рассчетного счета
+  if in_acc_section   
+            case req
+            when "ДатаНачала" then temporary_account[:interval_begin] = req_value
+            when "ДатаКонца" then  temporary_account[:interval_end]   = req_value
+            when "РасчСчет" then temporary_account[:number_acc] = req_value
+            when "НачальныйОстаток" then temporary_account[:begin_cash] = req_value
+            when "ВсегоПоступило" then temporary_account[:all_plus] = req_value
+            when "ВсегоСписано" then temporary_account[:all_minus] = req_value
+            when "КонечныйОстаток" then  temporary_account[:balance] = req_value
+            end
+        #конец варианта секции рассчетного счета
+  elsif unless do_not_parse_header
+  case req
+      when "ВерсияФормата" then @parameters[:format_version] = req_value
+      when "Кодировка"     then @parameters[:encoding] = req_value
+      when "Получатель"    then @parameters[:recipient_program] = req_value  
+      when "Отправитель"   then @parameters[:sender] = req_value
+      when "ДатаСоздания"  then @parameters[:create_date] = req_value
+      when "ВремяСоздания" then @parameters[:create_time] = req_value
+      when "ДатаНачала"    then @parameters[:date_begin]  = req_value
+      when "ДатаКонца"     then @parameters[:date_end] = req_value
+      when "РасчСчет"      then @accounts[req_value.to_s] = {:accnum => req_value} 
+        #строк РасчСчет может быть несколько, такая же встречается в секции СекцияРасчСчет, надо проверять флаг        
+      end    
+  end    
+   end      
+
+ else #если встречаем одиночные директивы секций     
+      case current_string 
+        when "СекцияРасчСчет" then in_acc_section = true
+        when "КонецРасчСчет"  then in_acc_section = false        
+      end       
+      do_not_parse_header = true if current_string == "СекцияРасчСчет" #флаг чтобы хедер не перезаписывался
+      #перебрасываем готовую секцию рассчетного счета если наткнулись на конец такой секции:      
+      if current_string == "КонецРасчСчет"
+      @accounts[temporary_account[:number_acc].to_s][:parameters] = Hash.new
+      @accounts[temporary_account[:number_acc].to_s][:parameters] = temporary_account.clone                          
+      temporary_account.clear #очищаем для страховки хеш чтобы старые данные не вкраплялись в новую секцию расс. счета                           
+      end
+end
+}
 end
 end
 
-def format_valid? #проверяет 1 строку на валидность формату 1С по соглашению
+def print_debug_state #отладочный метод
   
 end
 
-end
 
+end
--- spec/parser1C_spec.rb
+++ spec/parser1C_spec.rb
@@ -23,9 +23,51 @@ describe Parser1C do
   context "Наличие обязательных параметров формата обмена" do
     it 'Версия формата обязательна' do
       parser = Parser1C.new('1c_format_demo.txt')
-      parser.format_version.should be_false
-    #этот тест специально сделан непроходимым. Нужен способ как выяснить подается ли пустая строка или Nil
-    #в ожидателе
+      parser.parameters[:format_version].should_not == "" 
+    end
+       it 'Кодировка обязательна' do
+      parser = Parser1C.new('1c_format_demo.txt')
+      parser.parameters[:encoding].should_not == ""      
+    end
+         it 'Программа - получатель обязательна' do
+      parser = Parser1C.new('1c_format_demo.txt')      
+      parser.parameters[:recipient_program].should_not == ""      
+    end
+  end
+  
+  context "Правильность логики парсинга" do
+    it 'Баланс рассчетного счета должен браться' do
+      parser = Parser1C.new('1c_format_demo.txt')
+      parser.accounts["10203450600780000090"][:parameters][:balance].should == "90001.00"     
+    end  
+  end
+
+  context "Отладочный вывод информации для сверки" do
+    it "Выводим информацию о документе" do
+      parser = Parser1C.new('1c_format_demo.txt')
+      puts "###################################################################"
+      puts "Номер версии формата обмена : #{parser.parameters[:format_version]}"
+      puts "Кодировка файла :             #{parser.parameters[:encoding]}"
+      puts "Программа-получатель :        #{parser.parameters[:recipient_program]}"
+      puts "Отправитель :                 #{parser.parameters[:sender]}"
+      puts "Дата формирования файла  :    #{parser.parameters[:create_date]}"
+      puts "Время формирования файла :    #{parser.parameters[:create_time]}"
+      puts "Дата начала интервала    :    #{parser.parameters[:date_begin]}"
+      puts "Дата конца интервала     :    #{parser.parameters[:date_end]}"
+      
+      parser.accounts.keys.each { |key|
+        puts "Присутствует рассчетный счет : #{key}"
+        puts "--------------------------счет #{key}----------------------------"
+        puts "Дата начала интервала :              #{parser.accounts[key.to_s][:parameters][:interval_begin]}" 
+        puts "Дата конца  интервала :              #{parser.accounts[key.to_s][:parameters][:interval_end]}"
+        puts "Начальный остаток :                  #{parser.accounts[key.to_s][:parameters][:begin_cash]}"
+        puts "Обороты входящих платежей :          #{parser.accounts[key.to_s][:parameters][:all_plus]}"
+        puts "Обороты исходящих платежей :         #{parser.accounts[key.to_s][:parameters][:all_minus]}"
+        puts "Конечный остаток :                   #{parser.accounts[key.to_s][:parameters][:balance]}"
+        puts "---------------------конец счета #{key}--------------------------"
+        }
+      
+      puts "###################################################################"
     end
   end
 
